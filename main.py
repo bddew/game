c44a0cce4fdd30e6a678ce50f50dbdd3d3c5926d
bddew <pedrohenriquelangededsousa@gmail.com>
2025-05-27 18:39:36 -0300

    Novas Linhas!!

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pygame
 
+#Iniciando o PyGame e Criando uma janela
+pygame.init()
+display = pygame.display.set_mode([840, 480])
+pygame.display.set_caption("Jogo MEI T4")
+
+
+def draw():
+    display.fill([19, 173, 235])
+
+gameLoop = True
+isPressingW = False
 if __name__ == "__main__":
-    print("Funciona!!")
+    while gameLoop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameLoop = False
+            elif  event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    print("Apertou o W!")
+
+        draw()
+        pygame.display.update()
